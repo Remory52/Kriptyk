f4fd8cf66c7b31825a7089228c126e7b48ee6030
Remory52 <gondazsombor52@gmail.com>
2022-02-13 06:46:04 +0100

    Removed features

--- general.py
+++ general.py
@@ -2,19 +2,10 @@ import os
 import sys
 import discord
 from discord.ext import commands
-import urllib.request
-import requests
 
 class general(commands.Cog):    
     def __init__(self, client):
         self.client = client
-
-    @commands.command(name = "ip", aliases=["get_ip"], help="Get the Server's IP Address.")
-    async def ip(self, ctx):
-        if str(ctx.author) == str(os.getenv("owner")):
-            await ctx.send("Server's IP address is: " + urllib.request.urlopen('http://ident.me').read().decode('utf8'))
-        else:
-            await ctx.send("You are not authorized to execute this command!")
 
     @commands.command(name = "kill", aliases=["shutdown", "sh"], help="Shutdown Kriptyk.")
     async def kill(self, ctx):
@@ -48,21 +39,6 @@ class general(commands.Cog):
             await ctx.send(msg)
         else:
             await ctx.send(file=discord.File("gifs/nice_try.gif"))
-
-    @commands.command(name = "heat", aliases=["heater", "radiator"], help="Turn on or off the heating.")
-    async def heat(self, ctx, state : str):
-        if str(ctx.author) == str(os.getenv("owner")):
-            URL = "http://192.168.0.120/gpio/"
-
-            try:
-                if state.lower() == "on":
-                    requests.post(URL + str(1))
-                if state.lower() == "off":
-                    requests.post(URL + str(0))
-            except:
-                pass
-        else:
-            await ctx.send("You are not authorized to execute this command!")
 
 def setup(client):
     client.add_cog(general(client))
